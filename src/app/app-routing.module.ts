1dbabe3a1ffb9d1d44f9ac951f2effe0dc8fb570
dammugopi <venkatanagagopidammu@gmail.com>
2024-09-30 18:05:46 +0530

    Gopi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { OutputComponent } from './output/output.component';
-  import { GopiComponent } from './gopi/gopi.component';
-import { LoginComponent } from './login/login.component';
+ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path:'',component:RegisterComponent },
    {path:'out',component:OutputComponent },
-   {path:'gopi',component:GopiComponent},
-{path:"login",component:LoginComponent}
+ {path:"login",component:LoginComponent}
 ];
 
 @NgModule({
